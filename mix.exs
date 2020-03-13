3656704f11e95ea8335c320626b527d02e297b38
Eric Steen <ericsteen1@gmail.com>
2020-03-12 19:57:30 -0700

    Add dialyzer and ex_doc

--- mix.exs
+++ mix.exs
@@ -22,8 +22,8 @@ defmodule Automata.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
 end
