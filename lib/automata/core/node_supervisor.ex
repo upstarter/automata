11f347d704f9b69588399b8807bca231043ed375
Paul Tannenbaum <paultannenbaum@gmail.com>
2020-03-13 18:52:35 -0700

    Get all compile warnings to go away

--- lib/automata/core/node_supervisor.ex
+++ lib/automata/core/node_supervisor.ex
@@ -8,7 +8,7 @@ defmodule Automaton.NodeSupervisor do
     )
   end
 
-  def init([automaton_server, {m, _f, a}, name]) do
+  def init([automaton_server, {_m, _f, _a}, _name]) do
     Process.link(automaton_server)
 
     opts = [
@@ -20,7 +20,7 @@ defmodule Automaton.NodeSupervisor do
     DynamicSupervisor.init(opts)
   end
 
-  def child_spec([[automaton_server, {m, _f, a}, name]] = args) do
+  def child_spec([[_automaton_server, {_m, _f, _a}, name]] = args) do
     %{
       id: name <> "NodeSupervisor",
       start: {__MODULE__, :start_link, args},
