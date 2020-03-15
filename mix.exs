a440267d1a7543d96ea6d81e4043f0ec4952e7f7
Eric Steen <ericsteen1@gmail.com>
2020-03-15 14:41:07 -0700

    Testing setup

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule Automata.MixProject do
   defp deps do
     [
       {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
-      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:ex_spec, "~> 2.0", only: :test}
     ]
   end
 end
